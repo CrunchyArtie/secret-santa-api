1e16b7ac3e4ddbb71086231d803efa2ee4f621f3
CrunchyArtie <lundixhuit@gmail.com>
2021-12-12 12:01:57 +0100

    tirage au sort en dur

--- middleware/api-auth.js
+++ middleware/api-auth.js
@@ -2,12 +2,15 @@ const jwt = require("jsonwebtoken");
 const ApiResponse = require("../response");
 
 const apiAuth = (req, res, next) => {
-    const token =
-        req.body.token || req.query.token || req.headers["x-access-token"];
+    const rawToken =
+        req.body.token || req.query.token || req.headers["x-access-token"] || req.headers["authorization"];
 
-    if (!token) {
+    if (!rawToken) {
         return res.status(403).send(new ApiResponse(null, ["L'utilisateur doit être connecté."]));
     }
+
+    const token = rawToken.startsWith("Bearer ") ? rawToken.replace('Bearer ') : rawToken;
+
     try {
         jwt.verify(token, process.env.JWT_PLATFORM_TOKEN, {}, (decoded) => {
             req.user = decoded;
--- routes/api/lucky-friend.js
+++ routes/api/lucky-friend.js
@@ -2,11 +2,29 @@ const express = require('express');
 const apiAuth = require("../../middleware/api-auth");
 const ApiResponse = require("../../response");
 const router = express.Router();
-const db = require("../../models/index");
+
+
+// A ne pas faire :
+// Romane - Ade
+// Ade - Justine
+// Justine - Romane
+// Marine - Florence
+// Florence - Faustine
+// Faustine - Marine
+
+
+const mappingEnDur = {
+    'Romanou': 'Flopichou',
+    'Flopichou': 'Sirop d\'Erable',
+    'Sirop d\'Erable': 'Marine',
+    'Marine': 'Adé',
+    'Adé': 'Ylladia',
+    'Ylladia': 'Romanou',
+}
 
 /* GET users listing. */
-router.get('/', apiAuth, (req, res, next) => {
-  res.send(new ApiResponse('Romane'));
+router.get('/:username', apiAuth, (req, res, next) => {
+    res.send(new ApiResponse(mappingEnDur[req.params.username]));
 });
 
 module.exports = router;
